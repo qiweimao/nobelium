e64051da03295b0693def6da72125fc2f38789c8
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-07 19:21:05 -0400

    updated description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Qiwei Mao',
   email: 'rescue_tows0g@icloud.com',
   link: 'https://blog.qiweimao.dev/',
-  description: 'Qiwei Mao - Blog',
+  description: 'A Geotechnical Engineer\'s Journey into IoT',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'US/Eastern', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
