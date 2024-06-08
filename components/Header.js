48fc5793b5298bdb85bff2ab923b7136c33924e9
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-08 02:15:59 -0400

    nav remove rss add home

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,9 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
+    { id: 2, name: locale.NAV.HOME, to: 'https://www.qiweimao.dev/', show: true, external: false },
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
