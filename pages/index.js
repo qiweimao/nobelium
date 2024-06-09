976ecbd3f5e67df6988b0b0e769eccbd29d8dedd
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-09 14:43:57 -0400

    add container to home page

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,85 @@ export async function getStaticProps () {
 }
 
 const Portfolio = () => {
+  const BLOG = useConfig()
+  const layout = "blog"
+  const fullWidth = true
+
+  const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link
+  const meta = {
+    title: BLOG.title,
+    type: 'website',
+    ...customMeta
+  }
+
   return (
     <div>
-      <h1>My Portfolio</h1>
-      <p>Welcome to my portfolio page. Test</p>
-      {/* Add more portfolio content here */}
+      <Head>
+        <title>title</title>
+        {/* <meta content={BLOG.darkBackground} name="theme-color" /> */}
+        <meta name="robots" content="follow, index" />
+        <meta charSet="UTF-8" />
+        {BLOG.seo.googleSiteVerification && (
+          <meta
+            name="google-site-verification"
+            content={BLOG.seo.googleSiteVerification}
+          />
+        )}
+        {BLOG.seo.keywords && (
+          <meta name="keywords" content={BLOG.seo.keywords.join(', ')} />
+        )}
+        <meta name="description" content={meta.description} />
+        <meta property="og:locale" content={BLOG.lang} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta
+          property="og:url"
+          content={meta.slug ? `${url}/${meta.slug}` : url}
+        />
+        <meta
+          property="og:image"
+          content={`${BLOG.ogImageGenerateURL}/${encodeURIComponent(
+            meta.title
+          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
+        />
+        <meta property="og:type" content={meta.type} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta
+          name="twitter:image"
+          content={`${BLOG.ogImageGenerateURL}/${encodeURIComponent(
+            meta.title
+          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
+        />
+        {meta.type === 'article' && (
+          <>
+            <meta
+              property="article:published_time"
+              content={meta.date}
+            />
+            <meta property="article:author" content={BLOG.author} />
+          </>
+        )}
+      </Head>
+      <div
+        className={`wrapper ${BLOG.font === 'serif' ? 'font-serif' : 'font-sans'
+          }`}
+      >
+        <Header
+          navBarTitle={layout === 'blog' ? meta.title : null}
+          fullWidth={fullWidth}
+        />
+        <main className={cn(
+          'flex-grow transition-all',
+          layout !== 'blog' && ['self-center px-4', fullWidth ? 'md:px-24' : 'w-full max-w-2xl']
+        )}>
+          <p>12345 Test</p>
+        </main>
+        <Footer fullWidth={fullWidth} />
+      </div>
     </div>
-  );
+  )
 };
 
 export default Portfolio;
