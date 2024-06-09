8a633424933268866dea85457fbceca19d33b88b
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-09 15:53:50 -0400

    add blog layout

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export async function getStaticProps () {
 
 const Portfolio = () => {
   const BLOG = useConfig()
-  const layout = ""
+  const layout = "blog"
   const fullWidth = true
 
   const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link
