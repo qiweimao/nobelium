0f3e78c8fae3af3960a1989e6b682f4ea25ff1db
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-12 12:06:44 -0400

    updated badge for vertical alighnment

--- pages/index.js
+++ pages/index.js
@@ -33,13 +33,15 @@ export async function getStaticProps () {
   }
 }
 
-function Badge(props) {
+function Badge({ children, ...props }) {
   return (
     <a
       {...props}
       target="_blank"
       className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
-    />
+    >
+      {children}
+    </a>
   );
 }
 
