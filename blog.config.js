d10137cc6b9ce085cba7bab2f902d354eb44467c
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-12 11:58:01 -0400

    removed long description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Qiwei Mao',
   email: 'rescue_tows0g@icloud.com',
   link: 'https://blog.qiweimao.dev/',
-  description: 'A Geotechnical Engineer\'s Journey into IoT. Hi, I\'m Qiwei Mao, a geotechnical engineer with a passion for IoT systems. I\'m exploring low-power microcontrollers and LoRa communication systems to enable both hobbyist remote monitoring solutions and industrial-grade monitoring or control systems.',
+  description: 'A Geotechnical Engineer\'s Journey into IoT.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'US/Eastern', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
