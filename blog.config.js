8fef84e1f32b5627b7855d4602ff9e45c0f3ef8c
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-06-27 11:13:04 -0400

    update link address

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'QIWEI MAO',
   author: 'Qiwei Mao',
   email: 'rescue_tows0g@icloud.com',
-  link: 'https://blog.qiweimao.dev/',
+  link: 'https://www.qiweimao.dev/',
   description: 'A Geotechnical Engineer\'s Journey into IoT.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'US/Eastern', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
