3ca160d4ee97c88e12dfc47b3ccac3fb8a6cb462
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-07-12 16:53:12 -0400

    remove specivied height

--- components/YouTubeEmbed.js
+++ components/YouTubeEmbed.js
@@ -4,8 +4,8 @@ import styles from './YouTubeEmbed.module.css';
 const YouTubeEmbed = ({ embedId }) => (
   <div className={styles.videoResponsive}>
     <iframe
-      width="560"
-      height="315"
+      // width="560"
+      // height="315"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
