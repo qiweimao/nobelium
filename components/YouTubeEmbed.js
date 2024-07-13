a98054957c0d7d291911aae3af8ca7b2253d93b9
Qiwei Mao <57202568+qiweimao@users.noreply.github.com>
2024-07-13 00:34:01 -0400

    new build for youtube

--- components/YouTubeEmbed.js
+++ components/YouTubeEmbed.js
@@ -10,7 +10,7 @@ const YouTubeEmbed = ({ embedId }) => (
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-      title="Embedded YouTube"
+      title="Qiwei Mao's YouTube"
     />
   </div>
 );
